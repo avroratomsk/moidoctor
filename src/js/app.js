1cf84fa23129f75f9897be3da70c5238601f0f0f
sanchyopas <saniagolovanev@gmail.com>
2025-04-22 09:14:21 +0700

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,27 @@ import * as functions from './modules/baseFunctions.js';
 
 functions.isWebp();
 
+// new VenoBox({
+//   selector: '.my-image-links',
+//   numeration: true,
+//   infinigall: true,
+//   share: true,
+//   spinner: 'rotating-plane'
+// });
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
 new VenoBox({
   selector: '.my-image-links',
   numeration: true,
@@ -9,3 +30,4 @@ new VenoBox({
   share: true,
   spinner: 'rotating-plane'
 });
+
